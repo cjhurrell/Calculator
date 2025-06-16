826e978546edaaf7944065eaacdecc653280b4af
Chris H <cjhurrell0@gmail.com>
2025-06-16 10:27:46 +0100

    added in clear and back

--- calc.js
+++ calc.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const display = document.querySelector(".display");
 
-  document.querySelectorAll("button").forEach(button => {
+  document.querySelectorAll("button:not(#clear):not(#back)").forEach(button => {
     button.addEventListener("click", () => {
       const char = button.textContent;
-      display.textContent = char;
+      display.textContent += char;
     });
   });
+
+
+document.querySelector("#clear").addEventListener("click", () => {
+  const display = document.querySelector(".display");
+  display.textContent = "0";
+});
+
+document.querySelector("#back").addEventListener("click", () => {
+  const display = document.querySelector(".display");
+  display.textContent = display.textContent.slice(0, -1) || "0";
+});
 });
 
